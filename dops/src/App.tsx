1237aa193e5cfb90e66aff624d6cbc6dbca99342
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-07-12 21:50:30 +0300

    Updated file

--- dops/src/App.tsx
+++ dops/src/App.tsx
@@ -1,16 +1,60 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.css'
+import {Input} from "./components/Input";
 
-
-
-
-
-function App() {
-    return (
-        <div></div>
-    );
+type PropsType = {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
 }
 
+function App() {
+
+
+    const [todos, setTodos] = useState<PropsType[]>([])
+
+    const responseTodos = () => {
+            fetch('https://jsonplaceholder.typicode.com/todos')
+                .then(response => response.json())
+                .then(json => setTodos(json))
+    }
+
+    useEffect(() => {
+        responseTodos()
+    }, [])
+
+
+    const addTodos = () => responseTodos()
+
+    const deleteTodos = () => {
+        setTodos([])
+    }
+
+    const addTodo = () => {
+        const newTodo = {userId: 201, id: 2023, title: 'NEW TODO', completed: false}
+        return setTodos([newTodo, ...todos])
+    }
+
+    return (
+        <div className='App'>
+            <button onClick={addTodos}>ADD NEW DATA</button>
+            <button onClick={deleteTodos}>Delete Todos</button>
+            <Input callback={addTodo}/>
+
+            {todos.map((el) => {
+                return (
+                    <ol>
+                        <span>{el.userId} - </span>
+                        <span>{el.id} </span>
+                        <span>{el.title} </span>
+                        <input type='checkbox' checked={el.completed}/>
+                    </ol>
+                )
+            })}
+        </div>
+    );
+}
 
 
 export default App;
