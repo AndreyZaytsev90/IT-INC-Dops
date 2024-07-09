c274c401b28fb92b4fd3e966fcec0f654c62bce5
zaytsev <zaicev@mniti.ru>
2024-07-09 17:02:16 +0300

    dop-1

--- dops/src/App.tsx
+++ dops/src/App.tsx
@@ -1,69 +1,53 @@
-import React, {useEffect, useRef, useState} from 'react';
-import './App.css'
-import {Input} from "./components/Input";
-import {Button} from "./components/Button";
+import React, {useState} from 'react';
+import './App.css';
+import {Country} from "./Country";
 
-type PropsType = {
-    userId: number,
-    id: number,
-    title: string,
-    completed: boolean
+// На собесе от меня буду ждать моих размышлений, а не готовый результат
+
+export type BanknotsType = 'Dollars' | 'RUBLS'| 'All'// создадим типы для banknotes -он может быть 'Dollars', 'RUBLS' или 'All'
+export type MoneyType = {
+    banknotes: BanknotsType
+    value: number// не ленимся, убираем заглушку, и пишем правильный тип)
+    number: string// ложку за Димыча, за...
+}
+
+let defaultMoney: MoneyType[] = [  // типизируем
+    {banknotes: 'Dollars', value: 100, number: ' a1234567890'},
+    {banknotes: 'Dollars', value: 50, number: ' z1234567890'},
+    {banknotes: 'RUBLS', value: 100, number: ' w1234567890'},
+    {banknotes: 'Dollars', value: 100, number: ' e1234567890'},
+    {banknotes: 'Dollars', value: 50, number: ' c1234567890'},
+    {banknotes: 'RUBLS', value: 100, number: ' r1234567890'},
+    {banknotes: 'Dollars', value: 50, number: ' x1234567890'},
+    {banknotes: 'RUBLS', value: 50, number: ' v1234567890'},
+]
+
+// типизируем на входе и выходе
+export const moneyFilter = (money: MoneyType[], filter: BanknotsType): MoneyType[] => {
+    //если пришел filter со значением 'All', то возвращаем все банкноты
+    //return money.filter... ну да, придется фильтровать
+    return money.filter(m => m.banknotes === filter ? m : {...m, banknotes: filter})
 }
 
 function App() {
+    // убираем заглушки в типизации и вставляем в качестве инициализационного значения defaultMoney
+    const [money, setMoney] = useState<MoneyType[]>(defaultMoney)
+    const [filterValue, setFilterValue] = useState<BanknotsType>('All')   // по умолчанию указываем все банкноты
 
-
-    const [todos, setTodos] = useState<PropsType[]>([])
-    //const [newTitle, setNewTitle] = useState('')
-    const newTitle = useRef<HTMLInputElement>(null)
-
-
-    const responseTodos = () => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(response => response.json())
-            .then(json => setTodos(json))
-    }
-
-    useEffect(() => {
-        responseTodos()
-    }, [])
-
-
-    const addTodos = () => responseTodos()
-
-    const deleteTodos = () => {
-        setTodos([])
-    }
-
-    const addTodo = () => {
-        if(newTitle.current) {
-            const newTodo = {userId: todos.length + 1, id: todos.length + 1, title: newTitle.current.value, completed: false}
-            setTodos([newTodo, ...todos])
-            newTitle.current.value = ''
-        }
-        //setNewTitle('')
-    }
-
+    // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
+    // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
+    const filteredMoney = moneyFilter(money, filterValue)
     return (
-        <div className='App'>
-            <Button name={'Add Todos'} callback={addTodos}></Button>
-            <Button name={'Delete Todos'} callback={deleteTodos}></Button>
-            {/*<Input title={newTitle} setNewTitle={setNewTitle}/>*/}
-            <Input title={newTitle}/>
-            <Button name={'Add Todo'} callback={addTodo}></Button>
-            {todos.map((el) => {
-                return (
-                    <ol key={el.id}>
-                        <span>{el.userId} - </span>
-                        <span>{el.id} </span>
-                        <span>{el.title} </span>
-                        <input type='checkbox' checked={el.completed}/>
-                    </ol>
-                )
-            })}
+        <div className="App">
+            <Country
+                data={filteredMoney}   //отрисовать будем деньги после фильтрации
+                setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
+            />
         </div>
     );
 }
+
+// Итого: в этой компоненте у нас мозги. А вот отрисовка где-то глубже. Погружаемся в Country
 
 
 export default App;
