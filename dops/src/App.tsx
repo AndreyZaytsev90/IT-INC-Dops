09e930de9c09f2fb8f63e928c916a8da7c6df028
zaytsev <zaicev@mniti.ru>
2024-07-18 14:46:27 +0300

    SP1 dop 4 is done add: 1) SuperTodolist: React.FC<SuperTodolistProps> 2) children: React.ReactNode and {children}

--- dops/src/App.tsx
+++ dops/src/App.tsx
@@ -1,30 +1,74 @@
 import './App.css';
 import {SuperButton} from "./components/SuperButton/SuperButton";
 import s from "./components/SuperButton/superButton.module.css"
+import {SuperTodolist} from "./components/SuperTodolist/SuperTodolist";
 
-
+export type TasksProps = {id: number, title: string, isDone: boolean}
 
 function App() {
+
+    let tasks: TasksProps[] = [
+        {id: 1, title: "HTML", isDone: true},
+        {id: 2, title: "CSS", isDone: true},
+        {id: 3, title: "ES6/TS", isDone: false},
+        {id: 4, title: "REACT", isDone: false},
+    ]
 
 
     return (
         <div>
-       {/*    <SuperButton
+            {/*    <SuperButton
                onClick={()=> {}}
                name={"Blue Button"}
                className={s.stylesForSuperButton}/>*/}
-{/*
+            {/*
             <SuperButton
                onClick={()=> {}}
                name={"Red Button"}
                className={s.stylesForSuperButton2}/>*/}
 
-            <SuperButton
+            {/* <SuperButton
                 onClick={()=> {}}
                 //name={"Blue Button"}
                 className={s.stylesForSuperButton}>
                 Blue Button
-            </SuperButton>
+            </SuperButton>*/}
+
+            <SuperButton onClick={() => {
+            }} color={'blue'}>Blue Button</SuperButton>
+            <SuperButton onClick={() => {
+            }} color={'red'} disabled>Red Button</SuperButton>
+            <SuperButton onClick={() => {
+            }}>Default Button</SuperButton>
+
+            <SuperTodolist tasks={tasks}>
+                <SuperButton onClick={() => {}} color={'blue'}>Blue Button</SuperButton>
+                <SuperButton onClick={() => {}} color={'red'} disabled>Red Button</SuperButton>
+                <SuperButton onClick={() => {}}>Default Button</SuperButton>
+                <p>Привет!</p>
+                <p>Привет!</p>
+                <p>Привет!</p>
+                <input type="text"/>
+            </SuperTodolist>
+
+            <SuperTodolist tasks={tasks}>
+                <SuperButton onClick={() => {}}>Default Button</SuperButton>
+                <p>Привет!</p>
+                <p>Привет!</p>
+                <p>Привет!</p>
+            </SuperTodolist>
+
+            <SuperTodolist tasks={tasks}>
+                <input type="text"/>
+                <input type="text"/>
+                <p>Привет!</p>
+                <p>Привет!</p>
+                <input type="text"/>
+                <input type="text"/>
+                <p>Привет!</p>
+                <p>Привет!</p>
+                <input type="text"/>
+            </SuperTodolist>
         </div>
     );
 }
--- dops/src/components/SuperButton/SuperButton.tsx
+++ dops/src/components/SuperButton/SuperButton.tsx
@@ -1,9 +1,12 @@
 import React, {ButtonHTMLAttributes} from "react";
+import s from "./superButton.module.css"
 
 type SuperButtonProps = {
     onClick: () => void,
     children: React.ReactNode
     //name: string
+    color?: string
+    disabled?: boolean
 } & ButtonHTMLAttributes<HTMLButtonElement>
 export const SuperButton: React.FC<SuperButtonProps> = (props) => {
 
@@ -11,14 +14,45 @@ export const SuperButton: React.FC<SuperButtonProps> = (props) => {
         onClick,
         //name,
         children,
-        ...restProps} = props
+        color,
+        disabled,
+        ...restProps
+    } = props
 
     const onClickHandler = () => {
         onClick()
     }
 
+    /* // Ignat's жжот
+        const finalClassName = s.button
+            + (disabled
+                ? '' + s.disabled
+                : color === 'red'
+                    ? '' + s.red
+                    : color === 'secondary'
+                        ? '' + s.secondary
+                        : '' + s.default)
+            + (className ? ' ' + className : ' ')*/
+
+    //const finalClassName = s.button + " " + s.red //работает(все кнопки красные)
+
+    /*const finalClassName =
+        `${s.button}
+         ${color === 'red' ? s.red : s.secondary}` // работает*/
+
+    /*const finalClassName =
+         `${s.button}
+          ${color === 'red' ? s.red : s.secondary}
+          ${disabled ? s.disabled : ''}` // работает*/
+
+    const finalClassName =
+        `${s.button}
+          ${color === 'red' ? s.red : color === 'blue'? s.secondary : s.default} 
+          ${disabled ? s.disabled : ''}`
+
+
     return (
-        <button onClick={onClickHandler} {...restProps}>{children}</button>
+        <button onClick={onClickHandler} {...restProps} className={finalClassName}>{children}</button>
     );
 };
 
