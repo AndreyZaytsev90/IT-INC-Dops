08eeadaaad88d176a4bf0635bf564b78f4a8b1a6
zaytsev <zaicev@mniti.ru>
2024-08-14 16:45:28 +0300

    SP2 dop 8(routing2) continues

--- dops/src/App.tsx
+++ dops/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Site} from "./components/Site";
-import {NavLink, Outlet} from "react-router-dom";
+import {NavLink, Outlet, useNavigate} from "react-router-dom";
 import styles from "./components/Site.module.css";
 import {S} from "./components/pages/__styles";
 
@@ -13,22 +13,34 @@ function App() {
         console.log(logged)
     }
 
+    const navigate = useNavigate()
+
+    const comeBackHandler = () => {
+        navigate(-1)
+    }
+
+    const mainPageHandler = () => {
+        navigate('/page/0')
+    }
+
     return (
         <div>
             <div className={styles.header}><h1>HEADER</h1></div>
-           {/* <Site/>*/}
+            {/* <Site/>*/}
             <button onClick={onClickAuthHandler}>Auth</button>
             <div className={styles.body}>
                 <div className={styles.nav}>
                     {/*<div><NavLink className={({isActive})=>isActive ?styles.active:styles.navLink} to={'/page1'}>Page1</NavLink></div>*/}
-                    <S.NavWrapper><NavLink  to={'/page/0'}>Page1</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink  to={'/page/1'}>Page2</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink  to={'/page/2'}>Page3</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink  to={'/page/protected'}>ProtectedPage</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={'/page/0'}>Page1</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={'/page/1'}>Page2</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={'/page/2'}>Page3</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={'/page/protected'}>ProtectedPage</NavLink></S.NavWrapper>
                     <div><a href="/page1">aHrefPage1</a></div>
                 </div>
                 <div className={styles.content}>
                     {/*Outlet бывший Routes из Site*/}
+                    <button onClick={comeBackHandler}>НАЗАД</button>
+                    <button onClick={mainPageHandler}>ГЛАВНАЯ</button>
                     <Outlet/>
                 </div>
             </div>
