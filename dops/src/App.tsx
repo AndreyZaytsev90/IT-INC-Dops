6aa80efd4f978f43447ae432562baff0f8846eaa
zaytsev <zaicev@mniti.ru>
2024-08-15 10:18:00 +0300

    SP2 dop 8(routing2) is done

--- dops/src/App.tsx
+++ dops/src/App.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Site} from "./components/Site";
 import {NavLink, Outlet, useNavigate} from "react-router-dom";
 import styles from "./components/Site.module.css";
 import {S} from "./components/pages/__styles";
+import {useWindowSize} from "./helpers/useWindowSize";
 
 
 function App() {
@@ -23,20 +24,29 @@ function App() {
         navigate('/page/0')
     }
 
+    //const [burger, setBurger] = useState(true)
+
+    const windowSize = useWindowSize()
+    console.log(windowSize)
+
     return (
         <div>
             <div className={styles.header}><h1>HEADER</h1></div>
             {/* <Site/>*/}
             <button onClick={onClickAuthHandler}>Auth</button>
             <div className={styles.body}>
-                <div className={styles.nav}>
-                    {/*<div><NavLink className={({isActive})=>isActive ?styles.active:styles.navLink} to={'/page1'}>Page1</NavLink></div>*/}
-                    <S.NavWrapper><NavLink to={'/page/0'}>Page1</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={'/page/1'}>Page2</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={'/page/2'}>Page3</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={'/page/protected'}>ProtectedPage</NavLink></S.NavWrapper>
-                    <div><a href="/page1">aHrefPage1</a></div>
-                </div>
+                {windowSize < 1000
+                    ? <div></div>
+                    : <div className={styles.nav}>
+                        {/*<div><NavLink className={({isActive})=>isActive ?styles.active:styles.navLink} to={'/page1'}>Page1</NavLink></div>*/}
+                        <S.NavWrapper><NavLink to={'/page/0'}>Page1</NavLink></S.NavWrapper>
+                        <S.NavWrapper><NavLink to={'/page/1'}>Page2</NavLink></S.NavWrapper>
+                        <S.NavWrapper><NavLink to={'/page/2'}>Page3</NavLink></S.NavWrapper>
+                        <S.NavWrapper><NavLink to={'/page/protected'}>ProtectedPage</NavLink></S.NavWrapper>
+                        <div><a href="/page1">aHrefPage1</a></div>
+                    </div>
+                }
+
                 <div className={styles.content}>
                     {/*Outlet бывший Routes из Site*/}
                     <button onClick={comeBackHandler}>НАЗАД</button>
