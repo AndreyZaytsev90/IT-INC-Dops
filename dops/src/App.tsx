41e301baa1172d51baa2bbb03597bd23473a3b31
zaytsev <zaicev@mniti.ru>
2024-10-04 10:25:49 +0300

    CallBacks is done

--- dops/src/App.tsx
+++ dops/src/App.tsx
@@ -1,30 +1,46 @@
-import React, {useState} from 'react';
-import './App.css';
-import Component1 from "./components/Component1";
+import React from 'react';
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
+import {Error404} from "./components/pages/Error404";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import styles from "./components/Site.module.css";
+import {S} from './components/pages/_styles';
 
 
+const PATH = {
+    PAGE1: '/page1',
+    PAGE2: '/page2',
+    PAGE3: '/page3',
+} as const;
 
 function App() {
-    const [allGreen, setAllGreen] = useState(false);
-
-    const handleFinalCallback = () => {
-        setAllGreen(true);
-        alert("Первый круг Ада пройден!")
-    };
-
     return (
-        <div className="App">
-            <h1>Callback Hell</h1>
-            <div className="container">
-                <Component1
-                   /* onCallback={handleFinalCallback}*/
-                   onCallback={handleFinalCallback}
-                />
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <S.NavWrapper><NavLink to={PATH.PAGE1}>Page1</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2}>Page2</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3}>Page3</NavLink></S.NavWrapper>
+                    <a href="page3">page3 HTML</a>
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
+
+                        <Route path={PATH.PAGE1} element={<PageOne/>}/>
+                        <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+                        <Route path={PATH.PAGE3} element={<PageThree/>}/>
+
+                        <Route path={'/*'} element={<Error404/>}/>
+                    </Routes>
+                </div>
             </div>
-            {allGreen && <p>Первый круг Ада пройден!</p>}
+            <div className={styles.footer}>abibas 2024</div>
         </div>
     );
 }
 
-
 export default App;
+
